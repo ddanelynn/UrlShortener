34b226c85e853706e47217834b3cc66a782890db
ddanelynn <dingyoujia0823@gmail.com>
2022-09-20 16:03:12 +0800

    add new feature to get most popular link

--- server/index.js
+++ server/index.js
@@ -15,21 +15,40 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/:code', (req, res) => {
-    const sqlSelect = 'SELECT longUrl FROM url WHERE code = ?';
+    const sqlSelect = 'SELECT count, id, longUrl FROM url WHERE code = ?';
     db.query(sqlSelect, [req.params.code], (err, result) => {
         if (err) {
             console.log(err);
         } else {
+            const count = result[0].count + 1;
+            const sqlUpdate = 'UPDATE url SET count = ? WHERE id = ?';
+            db.query(sqlUpdate, [count, result[0].id], (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
             return res.redirect(result[0].longUrl);
         }
     })
 })
 
+app.post('/get-most-popular', (req, res) => {
+    const sqlReorder = 'SELECT shortUrl, count FROM url ORDER BY count DESC LIMIT 3;'
+    db.query(sqlReorder, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+
+})
+
 app.post('/api/shortener', (req, res) => {
     const code = shortId.generate();
     const shortUrl = 'http://localhost:3001/' + code;
-    const sqlInsert = `INSERT INTO url (code, longUrl, shortUrl) VALUES ('${code}', '${req.body.longUrl}', '${shortUrl}');`;
-    db.query(sqlInsert, (err, result) => {
+    const sqlInsert = `INSERT INTO url (code, longUrl, shortUrl, count) VALUES (?, ?, ?, 0);`;
+    db.query(sqlInsert, [code, req.body.longUrl, shortUrl], (err, result) => {
         if (err) {
             console.log(err);
         } else {
